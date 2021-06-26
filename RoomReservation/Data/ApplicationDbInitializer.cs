4634caa9f9b26f0df275e7fb1be81b5e7ca76fcc
Guillaume Fontorbe <gfontorbe@gmail.com>
2021-06-26 23:19:47 +0200

    Add roles Added Admin and User roles

---
+++ RoomReservation/Data/ApplicationDbInitializer.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoomReservation.Data
+{
+	public static class ApplicationDbInitializer
+	{
+		public static void Initialize(ApplicationDbContext context) {
+
+			if (!context.Roles.Any())
+			{
+				var roles = new IdentityRole[]
+				{
+					new IdentityRole{Name="Admin", NormalizedName="ADMIN"},
+					new IdentityRole{Name="User", NormalizedName="USER"}
+				};
+
+				context.Roles.AddRange(roles);
+
+				context.SaveChanges();
+			}
+		}
+	}
+}
