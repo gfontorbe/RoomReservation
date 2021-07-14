842736cdbc877ff5c322b974b76cfe9bc46fe42d
Guillaume Fontorbe <gfontorbe@gmail.com>
2021-07-14 21:21:56 +0200

    Add users index view

---
+++ RoomReservation/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RoomReservation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoomReservation.Controllers
+{
+	[Authorize(Roles = "Admin")]
+	public class UsersController : Controller
+	{
+		private readonly UserManager<ApplicationUser> _userManager;
+		private readonly RoleManager<IdentityRole> _roleManager;
+
+		public UsersController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+		{
+			_userManager = userManager;
+			_roleManager = roleManager;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			var users = await _userManager.Users.ToListAsync();
+
+			return View(users);
+		}
+	}
+}
