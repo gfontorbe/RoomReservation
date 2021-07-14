6c59dd9fa90b397e2055e55c59f34d782b4e63f6
Guillaume Fontorbe <gfontorbe@gmail.com>
2021-07-14 21:57:17 +0200

    Change display names for users table

--- RoomReservation/Models/UserViewModel.cs
+++ RoomReservation/Models/UserViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,9 +10,13 @@ namespace RoomReservation.Models
 	public class UserViewModel
 	{
 		public string Id { get; set; }
+		[DisplayName("First Name")]
 		public string FirstName { get; set; }
+		[DisplayName("Last Name")]
 		public string LastName { get; set; }
+		[DisplayName("Username")]
 		public string UserName { get; set; }
+		[DisplayName("Email address")]
 		public string Email { get; set; }
 		public IEnumerable<string> Roles { get; set; }
 	}
