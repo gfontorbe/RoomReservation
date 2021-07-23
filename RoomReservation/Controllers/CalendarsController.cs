cf01246930c5863f416944500ea58550ce3ce65f
Guillaume Fontorbe <gfontorbe@gmail.com>
2021-07-23 22:41:14 +0200

    Add calendar test view

---
+++ RoomReservation/Controllers/CalendarsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoomReservation.Controllers
+{
+	public class CalendarsController : Controller
+	{
+		public IActionResult Index()
+		{
+			// return View();
+
+			// View for tests
+			return View("Test");
+		}
+	}
+}
