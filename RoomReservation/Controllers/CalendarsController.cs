dd744811798ac424d10a8917d258b23e00c1356d
Guillaume Fontorbe <gfontorbe@gmail.com>
2021-07-31 23:02:06 +0200

    Update Reseravation Model and ViewModel

--- RoomReservation/Controllers/CalendarsController.cs
+++ RoomReservation/Controllers/CalendarsController.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGeneration.Contracts.ProjectModel;
+using RoomReservation.Data;
+using RoomReservation.Models;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,12 +13,47 @@ namespace RoomReservation.Controllers
 {
 	public class CalendarsController : Controller
 	{
+		private readonly UserManager<ApplicationUser> _userManager;
+		private readonly ApplicationDbContext _context;
+
+		public CalendarsController(UserManager<ApplicationUser> userManager, ApplicationDbContext context)
+		{
+			_userManager = userManager;
+			_context = context;
+		}
+
 		public IActionResult Index()
 		{
+
+
+			var resUser = _userManager.FindByEmailAsync("basic@company.com").Result;
+			var resRoom = _context.Rooms.Find("1e090611-c2df-47e2-90b9-70197b9888e3");
+
+			Reservation reservation = new Reservation()
+			{
+				Id = Guid.NewGuid().ToString(),
+				StartingTime = DateTime.Now,
+				Duration = 60,
+				ReservingUser = resUser,
+				ReservedRoom = resRoom
+			};
+
+			ReservationViewModel reservationViewModels = new ReservationViewModel()
+			{
+				Id = reservation.Id,
+				StartingTime = reservation.StartingTime,
+				EndingTime = reservation.EndingTime,
+				Title = $"{reservation.ReservingUser.FirstName} {reservation.ReservingUser.LastName}",
+				Editable = false,
+				DurationEditable = false,
+				Overlap = false
+			};
+
+
 			// return View();
 
 			// View for tests
-			return View("Test");
+			return View("Test", reservationViewModels);
 		}
 	}
 }
---
+++ RoomReservation/Models/ReservationViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoomReservation.Models
+{
+	public class ReservationViewModel
+	{
+		public string Id { get; set; }
+		public DateTime StartingTime { get; set;}
+		public DateTime EndingTime { get; set; }
+		public string Title { get; set; }
+		public bool Editable { get; set; }
+		public bool DurationEditable { get; set; }
+		public bool Overlap { get; set; }
+
+	}
+}
